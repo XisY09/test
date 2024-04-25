a35973b478c791c075f98ab81ddbb8981ecf34b0
XisY09 <muhsyafli755@gmail.com>
2024-04-25 09:50:57 +0800

    update

--- p2.php
+++ p2.php
@@ -45,11 +45,37 @@
         }
         button{
             background-color: transparent;
-    background-repeat: no-repeat;
-    border: none;
-    cursor: pointer;
-    overflow: hidden;
-    outline: none;
+            background-repeat: no-repeat;
+            border: none;
+            cursor: pointer;
+            overflow: hidden;
+            outline: none;
+            align-items: center;
+        }
+        .pagination {
+            list-style-type: none;
+            padding: 0;
+            margin: 20px 0;
+            display: flex;
+            justify-content: center;
+            align-items: center;
+        }
+        .pagination li {
+            margin: 0 5px;
+        }
+        .pagination li a {
+            text-decoration: none;
+            padding: 5px 10px;
+            border: 1px solid #ccc;
+            border-radius: 5px;
+            color: #333;
+        }
+        .pagination li a:hover {
+            background-color: #f0f0f0;
+        }
+        .pagination .current {
+            background-color: #007bff;
+            color: #fff;
         }
     </style>
   <header style="margin:10px 0 0 20px";>
@@ -65,8 +91,6 @@
             </div>
             </header>
 <div class="awal">
-<tr>
-
 <form>
   <div class="menu-container">
   <label for="option1.1">
@@ -87,6 +111,17 @@
     <input type="radio" id="option2.2" name="option" value="option2">
     ingin menuntut ilmu
   </div>
+    </div>
+  <div class="menu-container" style="width:100px;">
+        <ul class="pagination">
+            <li><a href="#">1</a></li>
+            <li><a href="#">2</a></li>
+            <li><a href="#">3</a></li>
+        </ul>
+    </div>
+    <div>
+    <button id="button">Simpan</button>
+    </div>
 </form>
 
 </body>
